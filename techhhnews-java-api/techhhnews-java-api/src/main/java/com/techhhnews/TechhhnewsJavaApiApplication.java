57fa2f0dabe131bd4a7b7ec03681df7f3c0377dc
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-07-26 18:47:24 -0400

    commit

---
+++ techhhnews-java-api/techhhnews-java-api/src/main/java/com/techhhnews/TechhhnewsJavaApiApplication.java
@@ -0,0 +1,13 @@
+package com.techhhnews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechhhnewsJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechhhnewsJavaApiApplication.class, args);
+	}
+
+}
---
+++ techhhnews-java-api/techhhnews-java-api/src/test/java/com/techhhnews/TechhhnewsJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techhhnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechhhnewsJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
